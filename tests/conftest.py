c4213cd03bd5efd5639c908261a6806de55573cc
Дмитрий Шапченко <dmitryshapchenko@yandex.ru>
2023-11-07 18:02:54 +0300

    headless add

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
     driver_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=driver_service)
+    driver = webdriver.Chrome(service=driver_service, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
